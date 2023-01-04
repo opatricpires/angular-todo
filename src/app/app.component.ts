9d34220b907d380a6e05bae3a7ab43aba6d6c0a5
opatricpires <patric.pires@outlook.com>
2023-01-04 13:03:16 -0300

    feat: show tasks in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Lista de tarefas';
+  tasks = [
+    {
+      id: 0,
+      taskName: 'Lavar louça',
+      taskDescription: 'Preciso lavar a louça urgente',
+    },
+    {
+      id: 1,
+      taskName: 'Tirar o lixo',
+      taskDescription: '',
+    },
+    {
+      id: 2,
+      taskName: 'Pagar as contas',
+      taskDescription: 'Pagar as contas no banco',
+    },
+  ];
+  subtitle = `Você tem um total de ${this.tasks.length} tarefas para hoje.`;
 }
